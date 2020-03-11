b076d12c1791ceaaa0129641a62d663eeb087be6
German Cruz <cruzgerman216@yahoo.com>
2020-03-11 20:27:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,11 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+<<<<<<< HEAD
     else
       puts "You are too old for this."
+=======
+>>>>>>> 9f27c0bf50c433b0b10ad7cf64de04684847b011
     end
   end
 end
